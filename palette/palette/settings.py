5d48aaec7251a195f5015e54882fa14aad00930a
Faiqah Shukor <58141681+nfaiqahh@users.noreply.github.com>
2021-03-03 12:06:53 +0800

    static files setting

--- palette/palette/settings.py
+++ palette/palette/settings.py
@@ -136,5 +136,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 # Login URL
 LOGIN_URL = 'mainmenu'
