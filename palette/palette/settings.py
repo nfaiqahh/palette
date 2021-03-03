4f7aeb893c5eb80bddef88366ffd65c0d034861a
Faiqah Shukor <58141681+nfaiqahh@users.noreply.github.com>
2021-03-03 11:35:36 +0800

    edit settings.py

--- palette/palette/settings.py
+++ palette/palette/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = '9e_n4!e553zon9o-q*zsz6duppc1i02luc=eoh0=y64%$(s645'
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'palette-analytics.herokuapp.com'
+    'palette-analytics.herokuapp.com',
+    '0.0.0.0',
+    '127.0.0.1'
 ]
 
 
